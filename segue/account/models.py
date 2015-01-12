a73dc76a390f9d971e8d662476a3569a45b7cf86
Felipe Mobus <fmobus@gmail.com>
2015-01-12 18:08:35 -0200

    passwords are automatically hashed when stored in database

--- segue/account/models.py
+++ segue/account/models.py
@@ -1,3 +1,5 @@
+from sqlalchemy_utils.types.password import PasswordType
+
 from ..json import SQLAlchemyJsonSerializer
 from ..core import db
 
@@ -10,5 +12,5 @@ class Account(AccountJsonSerializer, db.Model):
     id       = db.Column(db.Integer, primary_key=True)
     email    = db.Column(db.Text)
     name     = db.Column(db.Text)
-    password = db.Column(db.Text)
+    password = db.Column(PasswordType(schemes=['pbkdf2_sha512']))
     role     = db.Column(db.Enum(*schema.ACCOUNT_ROLES))
