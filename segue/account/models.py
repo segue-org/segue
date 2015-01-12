bd3c33f37e22826840b614d317f9d25a916735cb
Felipe Mobus <fmobus@gmail.com>
2015-01-12 17:51:24 -0200

    tying schema and enums together into the model

--- segue/account/models.py
+++ segue/account/models.py
@@ -1,6 +1,8 @@
 from ..json import SQLAlchemyJsonSerializer
 from ..core import db
 
+import schema
+
 class AccountJsonSerializer(SQLAlchemyJsonSerializer):
     __json_public__ = [ 'id', 'email', 'role' ]
 
@@ -9,4 +11,4 @@ class Account(AccountJsonSerializer, db.Model):
     email    = db.Column(db.Text)
     name     = db.Column(db.Text)
     password = db.Column(db.Text)
-    role     = db.Column(db.Enum("user","operator","admin"))
+    role     = db.Column(db.Enum(*schema.ACCOUNT_ROLES))
--- segue/proposal/models.py
+++ segue/proposal/models.py
@@ -1,10 +1,12 @@
 from ..json import SQLAlchemyJsonSerializer
 from ..core import db
 
+import schema
+
 class Proposal(SQLAlchemyJsonSerializer, db.Model):
   id          = db.Column(db.Integer, primary_key=True)
   title       = db.Column(db.Text)
   summary     = db.Column(db.Text)
   full        = db.Column(db.Text)
   language    = db.Column(db.String(100))
-  level       = db.Column(db.String(100))
+  level       = db.Column(db.Enum(*schema.PROPOSAL_LEVELS))
