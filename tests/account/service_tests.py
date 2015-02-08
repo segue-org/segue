004ca89998a7d9371cdbfe29802ed00e330250bb
Felipe Mobus <fmobus@gmail.com>
2015-02-08 14:51:40 -0200

    merged login credential failure/success to a single test case

--- tests/account/service_tests.py
+++ tests/account/service_tests.py
@@ -39,18 +39,13 @@ class AccountServiceTestCases(SegueApiTestCase):
             self.service.create(new_one)
 
     def test_login_given_valid_credential(self):
-        account = ValidAccountFactory.build().to_json(all_fields=True)
-        account['password'] = 'password' # factory-boy can't keep SQLAlchemy from swallowing the value =/
-        mockito.when(self.mock_signer).sign(account).thenReturn()
-        saved = self.service.create(account)
+        account = self.create_from_factory(ValidAccountFactory, password="right")
 
-        result = self.service.login(email=account['email'], password="password")
-
+        result = self.service.login(email=account.email, password="right")
         mockito.verify(self.mock_signer).sign(account)
 
-    def test_login_raises_given_invalid_credentials(self):
-        account = ValidAccountFactory.build().to_json(all_fields=True)
-        account['password'] = 'password' # factory-boy can't keep SQLAlchemy from swallowing the value =/
+        with self.assertRaises(InvalidLogin):
+            self.service.login(email=account.email, password="wrong")
 
         with self.assertRaises(InvalidLogin):
-            self.service.login(email=account['email'], password="password")
+            self.service.login(email='random-email', password="right")
