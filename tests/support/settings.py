cda6192802bce9049c82a480dc3b3f38141b2441
Felipe Mobus <fmobus@gmail.com>
2015-03-14 21:11:56 -0300

    yet another attempt at fixing snap-ci

--- tests/support/settings.py
+++ tests/support/settings.py
@@ -1,3 +1,4 @@
+from datetime import timedelta
 DEBUG = False
 TESTING = True
 
@@ -7,3 +8,21 @@ SQLALCHEMY_POOL_SIZE = None
 SQLALCHEMY_POOL_TIMEOUT = None
 SQLALCHEMY_POOL_RECYCLE = None
 SQLALCHEMY_DATABASE_URI = 'sqlite:///:memory:'
+
+LOGFILE = '/var/log/segue/segue.log'
+
+FRONTEND_URL = 'http://localhost:9000'
+BACKEND_URL = 'http://192.168.33.91:9001'
+
+JWT_SECRET_KEY = 'sshh'
+JWT_DEFAULT_REALM = 'Login Required'
+JWT_AUTH_URL_RULE = None
+JWT_AUTH_ENDPOINT = None
+JWT_ALGORITHM     = 'HS256'
+JWT_VERIFY        = True
+JWT_LEEWAY        = 0
+JWT_VERIFY_EXPIRATION =  True
+JWT_EXPIRATION_DELTA = timedelta(days=30)
+
+CORS_HEADERS = 'Content-Type,Authorization'
+CORS_ORIGINS = '*'
