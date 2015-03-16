78f87ee6f8f4e733eed6cadaaf7d5ca9906adafe
Felipe Mobus <fmobus@gmail.com>
2015-03-15 22:42:34 -0300

    refactored pagseguro factory to delegate to a details factory

--- segue/purchase/pagseguro/factories.py
+++ segue/purchase/pagseguro/factories.py
@@ -17,27 +17,18 @@ class PagSeguroPaymentFactory(Factory):
         payment.reference = "A{0:05d}-PU{1:05d}".format(purchase.customer.id, purchase.id)
         return payment
 
-class PagSeguroSessionFactory(object):
-    def __init__(self, use_env=None):
-        self.use_env = use_env or config.PAGSEGURO_ENV
-        if not self.use_env: raise(BadConfiguration('No env set for pagseguro'))
-
-    def create_session(self, payment):
-        purchase = payment.purchase
-        buyer    = purchase.buyer
-        product  = purchase.product
-        customer = purchase.customer
-
-        pg = PagSeguro(config.PAGSEGURO_EMAIL, config.PAGSEGURO_TOKEN)
-        pg.config = ConfigSandbox
-        pg.sender = {
+class PagSeguroDetailsFactory(object):
+    def create_sender(self, customer, buyer):
+        return {
             "name":      buyer.name,
             "area_code": customer.phone[0:2].strip(),
             "phone":     customer.phone[2:].strip(),
             "email":     customer.email
         }
-        pg.shipping = {
-            'type':        pg.NONE,
+
+    def create_shipping(self, buyer):
+        return {
+            'type':        3,
             "street":      buyer.address_street,
             "number":      buyer.address_number,
             "complement":  buyer.address_extra,
@@ -45,17 +36,42 @@ class PagSeguroSessionFactory(object):
             'city':        buyer.address_city,
             "country":     buyer.address_country
         }
+
+    def create_item(self, payment, product):
+        return {
+            "id":          "0001",
+            "description": product.description,
+            "amount":      "{0:0.2f}".format(payment.amount),
+            "quantity":    1,
+            "weight":      0
+        }
+
+    def reference(self, payment):
+        return "{0}-PA{1:05d}".format(payment.reference, payment.id)
+
+    def redirect_url(self, payment, purchase):
+        return '{}/api/purchases/{}/payment/{}/conclude'.format(config.BACKEND_URL, purchase.id, payment.id)
+
+    def notification_url(self, payment, purchase):
+        return '{}/api/purchases/{}/payment/{}/notify'.format(config.BACKEND_URL, purchase.id, payment.id)
+
+class PagSeguroSessionFactory(object):
+    def __init__(self, use_env=None, details_factory=None):
+        self.use_env = use_env or config.PAGSEGURO_ENV
+        if not self.use_env: raise(BadConfiguration('No env set for pagseguro'))
+        self.details_factory = details_factory or PagSeguroDetailsFactory()
+
+    def create_session(self, payment):
+        pg = PagSeguro(config.PAGSEGURO_EMAIL, config.PAGSEGURO_TOKEN)
+        pg.config = ConfigSandbox
+
+        pg.sender    = self.details_factory.create_sender(payment.purchase.customer, payment.purchase.buyer)
+        pg.shipping  = self.details_factory.create_shipping(payment.purchase.buyer)
+        pg.items     = [ self.details_factory.create_item(payment, payment.purchase.product) ]
+
         pg.reference_prefix = "SEGUE-FISL16-"
-        pg.reference = "{0}-PA{1:05d}".format(payment.reference, payment.id)
+        pg.reference        = self.details_factory.reference(payment)
+        pg.redirect_url     = self.details_factory.redirect_url(payment, payment.purchase)
+        pg.notification_url = self.details_factory.notification_url(payment, payment.purchase)
 
-        pg.redirect_url     = '{}/api/purchases/{}/payment/{}/conclude'.format(config.BACKEND_URL, purchase.id, payment.id)
-        pg.notification_url = '{}/api/purchases/{}/payment/{}/notify'.  format(config.BACKEND_URL, purchase.id, payment.id)
-
-        pg.add_item(
-            id="0001",
-            description=product.description,
-            amount="{0:0.2f}".format(payment.amount),
-            quantity=1,
-            weight=0
-        )
         return pg
