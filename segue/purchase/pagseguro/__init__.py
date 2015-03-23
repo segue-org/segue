2400611092d8727eef8046fa74115158ed636f3e
Felipe Mobus <fmobus@gmail.com>
2015-03-22 21:58:57 -0300

    reorganizing code of pagseguro service to match the flow better

--- segue/purchase/pagseguro/__init__.py
+++ segue/purchase/pagseguro/__init__.py
@@ -8,6 +8,26 @@ class PagSeguroPaymentService(object):
     def __init__(self, sessions=None):
         self.sessions = sessions or PagSeguroSessionFactory()
 
+    def create(self, purchase, data=None):
+        payment = PagSeguroPaymentFactory.create(purchase)
+        db.session.add(payment)
+        db.session.commit()
+        return payment
+
+    def process(self, payment):
+        payment_session = self.sessions.payment_session(payment)
+        try:
+            logger.debug('starting pagseguro checkout for %s...', payment.id)
+            result = payment_session.checkout()
+            logger.debug('pagseguro answered with xml... %s', result.xml)
+            logger.debug('completed checkout for %s. result is: %s', payment.id, result.payment_url)
+            instructions = self._build_instructions(result)
+            payment.code = result.code
+            return instructions
+        except RequestException, e:
+            logger.error('connection error to pagseguro! %s', e)
+            raise ExternalServiceError('pagseguro')
+
     def notify(self, purchase, payment, payload=None, source='notification'):
         logger.debug('received pagseguro direct notification with payload %s', payload)
         notification_code = payload.get('notificationCode', None)
@@ -20,12 +40,6 @@ class PagSeguroPaymentService(object):
             logger.error('connection error to pagseguro! %s', e)
             raise ExternalServiceError('pagseguro')
 
-    def create(self, purchase, data=None):
-        payment = PagSeguroPaymentFactory.create(purchase)
-        db.session.add(payment)
-        db.session.commit()
-        return payment
-
     def conclude(self, payment, payload=None):
         logger.debug('received pagseguro conclude notification with payload %s', payload)
         transaction_id = payload.get('transaction_id', None)
@@ -34,20 +48,6 @@ class PagSeguroPaymentService(object):
         try:
             xml_result = self.sessions.query_session(transaction_id).check()
             return PagSeguroTransitionFactory.create(transaction_id, payment, xml_result, 'conclude')
-        except RequestException, e:
-            logger.error('connection error to pagseguro! %s', e)
-            raise ExternalServiceError('pagseguro')
-
-    def process(self, payment):
-        payment_session = self.sessions.payment_session(payment)
-        try:
-            logger.debug('starting pagseguro checkout for %s...', payment.id)
-            result = payment_session.checkout()
-            logger.debug('pagseguro answered with xml... %s', result.xml)
-            logger.debug('completed checkout for %s. result is: %s', payment.id, result.payment_url)
-            instructions = self._build_instructions(result)
-            payment.code = result.code
-            return instructions
         except RequestException, e:
             logger.error('connection error to pagseguro! %s', e)
             raise ExternalServiceError('pagseguro')
