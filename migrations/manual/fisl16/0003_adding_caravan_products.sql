829f3c65fcb43e05412713d4ad5c8de1d5faa9a0
Felipe Mobus <fmobus@gmail.com>
2015-04-08 02:47:48 -0300

    adding price column to caravan products manual migration

--- migrations/manual/fisl16/0003_adding_caravan_products.sql
+++ migrations/manual/fisl16/0003_adding_caravan_products.sql
@@ -1,19 +1,19 @@
 BEGIN TRANSACTION;
 
     INSERT INTO PRODUCT (kind, category, "public", price, sold_until, description)
-                 VALUES ('ticket', 'caravan', 'F', '2015-04-13 23:59:59', 'ingresso FISL16 - caravana - lote 1');
+                 VALUES ('ticket', 'caravan', 'F', 60,  '2015-04-13 23:59:59', 'ingresso FISL16 - caravana - lote 1');
 
     INSERT INTO PRODUCT (kind, category, "public", price, sold_until, description)
-                 VALUES ('ticket', 'caravan', 'F', '2015-04-20 23:59:59', 'ingresso FISL16 - caravana - lote 2');
+                 VALUES ('ticket', 'caravan', 'F', 85,  '2015-04-20 23:59:59', 'ingresso FISL16 - caravana - lote 2');
 
     INSERT INTO PRODUCT (kind, category, "public", price, sold_until, description)
-                 VALUES ('ticket', 'caravan', 'F', '2015-05-18 23:59:59', 'ingresso FISL16 - caravana - lote 3');
+                 VALUES ('ticket', 'caravan', 'F', 110, '2015-05-18 23:59:59', 'ingresso FISL16 - caravana - lote 3');
 
     INSERT INTO PRODUCT (kind, category, "public", price, sold_until, description)
-                 VALUES ('ticket', 'caravan', 'F', '2015-06-15 23:59:59', 'ingresso FISL16 - caravana - lote 4');
+                 VALUES ('ticket', 'caravan', 'F', 135, '2015-06-15 23:59:59', 'ingresso FISL16 - caravana - lote 4');
 
     INSERT INTO PRODUCT (kind, category, "public", price, sold_until, description)
-                 VALUES ('ticket', 'caravan', 'F', '2015-06-30 23:59:59', 'ingresso FISL16 - caravana - lote 5');
+                 VALUES ('ticket', 'caravan', 'F', 160, '2015-06-30 23:59:59', 'ingresso FISL16 - caravana - lote 5');
 
 COMMIT;
 
