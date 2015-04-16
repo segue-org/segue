478c420671ac6e4dbfa429844bb46a48593ee03d
Felipe Mobus <fmobus@gmail.com>
2015-04-16 00:11:42 -0300

    underping migration 0005

--- migrations/manual/fisl16/0005_extending_caravan_products.sql
+++ migrations/manual/fisl16/0005_extending_caravan_products.sql
@@ -1,10 +1,10 @@
 BEGIN TRANSACTION;
 
-    UPDATE caravan SET sold_until = '2015-04-26' WHERE category = 'caravan' AND price = 60;
-    UPDATE caravan SET sold_until = '2015-05-10' WHERE category = 'caravan' AND price = 85;
-    UPDATE caravan SET sold_until = '2015-05-18' WHERE category = 'caravan' AND price = 110;
-    UPDATE caravan SET sold_until = '2015-06-15' WHERE category = 'caravan' AND price = 135;
-    UPDATE caravan SET sold_until = '2015-06-30' WHERE category = 'caravan' AND price = 160;
+    UPDATE product SET sold_until = '2015-04-26' WHERE category = 'caravan' AND price = 60;
+    UPDATE product SET sold_until = '2015-05-10' WHERE category = 'caravan' AND price = 85;
+    UPDATE product SET sold_until = '2015-05-18' WHERE category = 'caravan' AND price = 110;
+    UPDATE product SET sold_until = '2015-06-15' WHERE category = 'caravan' AND price = 135;
+    UPDATE product SET sold_until = '2015-06-30' WHERE category = 'caravan' AND price = 160;
 
 COMMIT;
 
