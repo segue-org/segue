ef3cf0eea637ba78c6c51c65304b6ce866d5feff
Felipe Mobus <fmobus@gmail.com>
2015-04-15 21:33:01 -0300

    implemented lookup account, get account, list proposals by owner, get proposal

--- segue/admin/__init__.py
+++ segue/admin/__init__.py
@@ -21,29 +21,6 @@ def admin_only(fn):
         return fn(instance, *args, **kw)
     return wrapped
 
-class AccountDetailResponse(SimpleJson):
-    @classmethod
-    def create(cls, list_or_entity):
-        if isinstance(list_or_entity, list):
-            return [ cls(e) for e in list_or_entity ]
-        cls(list_or_entity)
-
-    def add_link(self, name, collection, route='', **route_parms):
-        if not hasattr(self, 'links'):
-            self.links = {}
-        self.links[name] = dict(
-            count=len(collection),
-            href =url_for(route, **route_parms)
-        )
-
-    def __init__(self, entity):
-        self.name = entity.name
-        self.email = entity.email
-        self.id = entity.id
-        self.add_link('proposals', entity.proposals, 'admin.proposals', owner_id   =entity.id)
-        self.add_link('caravans',  entity.proposals, 'admin.caravans',  owner_id   =entity.id)
-        self.add_link('purchases', entity.purchases, 'admin.purchases', customer_id=entity.id)
-        self.add_link('payments',  entity.payments,  'admin.payments',  customer_id=entity.id)
 
 class AdminController(object):
     def __init__(self, accounts=None, proposals=None):
