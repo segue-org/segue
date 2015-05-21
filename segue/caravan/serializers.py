40c6a19061327b2050b2625080b04ff966bb6112
Daniel Wolff <daniel@rapido.net.br>
2015-05-21 12:56:16 -0300

    added 'paid_riders' in extra_fields for the Caravan serializer

--- segue/caravan/serializers.py
+++ segue/caravan/serializers.py
@@ -2,9 +2,11 @@ from ..json import SQLAlchemyJsonSerializer
 
 class CaravanJsonSerializer(SQLAlchemyJsonSerializer):
     _serializer_name = 'normal'
-    _child_serializers = dict(owner='SafeAccountJsonSerializer')
+    _child_serializers = dict(owner='SafeAccountJsonSerializer', paid_riders='ShortCaravanInviteJsonSerializer')
     def serialize_child(self, child):
         return self._child_serializers.get(child, False)
+    def extra_fields(self):
+        return ['paid_riders']
 
 class CaravanInviteJsonSerializer(SQLAlchemyJsonSerializer):
     _serializer_name = 'normal'
@@ -15,6 +17,6 @@ class ShortCaravanInviteJsonSerializer(CaravanInviteJsonSerializer):
     _serializer_name = 'short'
 
     def hide_field(self, child):
-        return child not in ['name', 'recipient', 'status', 'paid']
+        return child not in ['name', 'recipient', 'status']
     def serialize_child(self, child):
-        return False;
+        return False
