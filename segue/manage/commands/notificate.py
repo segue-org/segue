a6f94a461366b86119dadc006941390d7ce265b1
Felipe Mobus <fmobus@gmail.com>
2015-06-19 00:29:59 -0400

    adding some color to call notification command

--- segue/manage/commands/notificate.py
+++ segue/manage/commands/notificate.py
@@ -1,7 +1,8 @@
 # encoding: utf-8
 import sys
-
+from collections import defaultdict
 from datetime import datetime
+
 from segue.proposal.services import ProposalService
 from segue.schedule.services import NotificationService
 from support import *;
@@ -12,14 +13,28 @@ def notify_proposals(deadline=None, start=0, end=sys.maxint):
     proposals = ProposalService().all_with_tags('marked','accepted')
     notification = NotificationService()
 
+    operations = defaultdict(lambda: 0)
+
     for proposal in proposals:
         if proposal.id < int(start): continue;
         if proposal.id > int(end): continue;
 
-        print "{} issuing notification call_proposal for proposal {}{}{} - {}{}{} ({}{}{})...".format(F.RESET,
-                F.GREEN, proposal.id, F.RESET,
-                F.GREEN, u(proposal.title), F.RESET,
-                F.RED, proposal.owner.email, F.RESET
-        ),
-        notification.call_proposal(proposal.id, deadline)
-        print F.GREEN + "OK"
+        try:
+            print "{} issuing notification call_proposal for proposal {}{}{} - {}{}{} ({}{}{})...".format(F.RESET,
+                    F.GREEN, proposal.id, F.RESET,
+                    F.GREEN, u(proposal.title), F.RESET,
+                    F.RED, proposal.owner.email, F.RESET
+            ),
+            notification.call_proposal(proposal.id, deadline)
+            print F.GREEN + "OK" + F.RESET
+            operations["OK"] += 1
+        except SegueError, e:
+            name = e.__class__.__name__
+            operations[name] += 1
+
+    print "==[ RESULTS ]========================="
+    print "{}{}{} notifications got sent".format(F.GREEN, operations.pop('OK'), F.RESET)
+    for key, value in operations.items():
+        print "{}{}{} notifications resulted in {}{}{}".format(F.GREEN, value, F.RESET, F.GREEN, key, F.RESET)
+
+
