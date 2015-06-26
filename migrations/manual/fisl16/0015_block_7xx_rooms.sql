080db01a21463e9af977cee27373c60418021e39
Felipe Mobus <fmobus@gmail.com>
2015-06-26 00:53:02 -0300

    removing migration to block 7xx rooms

--- migrations/manual/fisl16/0015_block_7xx_rooms.sql
+++
@@ -1,8 +0,0 @@
-BEGIN TRANSACTION;
-
-UPDATE slot SET blocked = 't' WHERE room_id IN (SELECT id FROM room where name like '712%');
-UPDATE slot SET blocked = 't' WHERE room_id IN (SELECT id FROM room where name like '713%');
-UPDATE slot SET blocked = 't' WHERE room_id IN (SELECT id FROM room where name like '714%');
-UPDATE slot SET blocked = 't' WHERE room_id IN (SELECT id FROM room where name like '715%');
-
-COMMIT;
