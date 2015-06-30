bb92805ba40817aeb3c11b4d66e01b7037e023d0
Felipe Mobus <fmobus@gmail.com>
2015-06-30 11:54:58 -0300

    added gives_kit, is_speaker and is_promo columns to product

--- segue/product/models.py
+++ segue/product/models.py
@@ -16,6 +16,9 @@ class Product(JsonSerializable, db.Model):
     price       = db.Column(db.Numeric)
     sold_until  = db.Column(db.DateTime)
     description = db.Column(db.Text)
+    is_promo    = db.Column(db.Boolean, server_default='f')
+    is_speaker  = db.Column(db.Boolean, server_default='f')
+    gives_kit   = db.Column(db.Boolean, server_default='t')
 
     purchases  = db.relationship("Purchase", backref="product")
 
