ca8f68f426cd1ca9171700b8a8c5eb97623103fb
Felipe Mobus <fmobus@gmail.com>
2015-07-02 11:49:53 -0300

    removed another room

--- migrations/manual/fisl16/0025_removed_some_rooms.sql
+++ migrations/manual/fisl16/0025_removed_some_rooms.sql
@@ -3,4 +3,7 @@ BEGIN TRANSACTION;
   DELETE FROM slot WHERE room_id IN (SELECT id FROM room WHERE name = 'PMPA');
   DELETE FROM room WHERE name = 'PMPA';
 
+
+  DELETE FROM slot WHERE room_id IN (SELECT id FROM room WHERE name = 'Terr. Ap. Livre');
+  DELETE FROM room WHERE name = 'Terr. App. Livre';
 COMMIT;
