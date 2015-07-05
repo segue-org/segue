176e79f034708c3149471b526eba972545e742be
Daniel Wolff <daniel@rapido.net.br>
2015-07-05 15:23:29 -0300

    added badge_name to account model

--- segue/account/models.py
+++ segue/account/models.py
@@ -33,6 +33,7 @@ class Account(JsonSerializable, db.Model):
     id           = db.Column(db.Integer, primary_key=True)
     email        = db.Column(db.Text, unique=True)
     name         = db.Column(db.Text)
+    badge_name   = db.Column(db.Text)
     password     = db.Column(PasswordType(schemes=['pbkdf2_sha512']))
     role         = db.Column(db.Enum(*schema.ACCOUNT_ROLES, name='account_roles'))
     document     = db.Column(db.Text)
