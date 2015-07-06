b69ce29cbca1c7f609b8b6ab8b475bee850b9e8d
Felipe Mobus <fmobus@gmail.com>
2015-07-06 15:28:48 -0300

    ops, forgot to add printer parameter

--- segue/manage/commands/printer.py
+++ segue/manage/commands/printer.py
@@ -74,7 +74,7 @@ def print_range(categories="", start=None, end=None, printer=None, failures_only
 
         print "... {}matches printing criteria{}, printing".format(F.GREEN, F.RESET)
         printed += 1
-        badges.make_badge(person.id)
+        badges.make_badge(printer, person.id)
 
     if rehearse:
         print "WOULD BE PRINTED", printed
