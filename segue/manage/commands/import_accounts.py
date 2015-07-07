042c1e86d6506e000dc715f93b99467fd83f3608
Felipe Mobus <fmobus@gmail.com>
2015-07-07 19:41:59 -0300

    improved import_accoutn script reporitng

--- segue/manage/commands/import_accounts.py
+++ segue/manage/commands/import_accounts.py
@@ -5,55 +5,67 @@ import tablib
 import json
 import yaml
 
+
 from segue.core import db
 from segue.hasher import Hasher
 from segue.models import *
-from segue.mailer import MailerService
 from segue.account.services import AccountService
+
+from support import *
 
 ds = tablib.Dataset()
 
-account_service = AccountService()
-mailer_service = MailerService()
 
-def import_accounts(in_file, product_id):
+def import_accounts(in_file, product_id, commit=False):
+    init_command()
+    accounts = AccountService()
+    hasher = Hasher(8)
+
+    skipped = 0
+    created = 0
+
     with open(in_file, "r") as f:
         ds.csv = f.read()
 
         product = Product.query.get(product_id)
 
         for line in ds.dict:
-            if not account_service.is_email_registered(line['email']):
-                account_data = {
-                    'name':     line['nome'],
-                    'document': line['cpf'],
-                    'email':    line['email'],
-                    'password': generate_password()
-                }
-                account = Account(**account_data)
+            name     = line['nome']
+            email    = line['email']
+            document = line['cpf']
+            password = hasher.generate()
+
+            print "scanning {}{}{}...".format(F.RED, email, F.RESET)
+
+            if accounts.is_email_registered(line['email']):
+                print "... {}had account already!{}".format(F.RED, F.RESET)
+                account = accounts.get_by_email(email)
+            else:
+                print "... {}creating account{}".format(F.GREEN, F.RESET)
+                account = Account(name=name, email=email, document=document)
                 db.session.add(account)
-            else:
-                account = Account.query.filter(Account.email == line['email']).first()
 
-
-            if not account.has_valid_purchases:
-                purchase_data = {
-                    'product': product,
-                    'customer': account,
-                    'status': 'paid',
-                    'kind': 'exempt'
-                }
-
-                purchase = Purchase(**purchase_data)
-                db.session.add(purchase)
-            else:
+            if account.has_valid_purchases:
+                print "... {}had purchase already!{}".format(F.RED, F.RESET)
                 purchase = account.identifier_purchase
+                skipped += 1
+                continue
+            else:
+                print "... {}creating purchase{}".format(F.GREEN, F.RESET)
+                purchase = Purchase(customer=account, product=product)
+                purchase.kind   = 'exempt'
+                purchase.status = 'paid'
+                db.session.add(purchase)
+                created += 1
 
-            try:
+        try:
+            print "results: skipped={}, created={}".format(skipped, created)
+            if commit:
+                print "comitting!"
                 db.session.commit()
-            except Exception, e:
-                print e.__dict__
-
-def generate_password():
-    h = Hasher(8)
-    return h.generate()
+            else:
+                print "rolling back"
+                db.session.rollback()
+        except Exception, e:
+            import traceback
+            traceback.print_exc()
