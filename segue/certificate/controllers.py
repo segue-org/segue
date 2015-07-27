2139441b7acf6aeba80dca28bb2e7720afb1451f
Felipe Mobus <fmobus@gmail.com>
2015-07-27 03:46:58 -0300

    fixing splitting of descriptor

--- segue/certificate/controllers.py
+++ segue/certificate/controllers.py
@@ -30,11 +30,11 @@ class CertificateController(object):
     def issue(self, account_id):
         if self.current_user.id != account_id: flask.abort(403)
         descriptor = request.get_json().get('descriptor', None) or flask.abort(400)
-        kind, id = descriptor.split("-")
+        kind = descriptor.split("-")[0]
 
         payload = dict(language=self.current_user.guessed_language)
         if kind == 'speaker':
-            payload['talk'] = self.proposals.get_one(id, strict=True)
+            payload['talk'] = self.proposals.get_one(descriptor.split("-")[1], strict=True)
 
         result = self.service.issue_certificate(self.current_user, kind, **payload)
 
